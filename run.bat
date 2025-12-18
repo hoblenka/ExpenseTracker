a5f7033f65d46554647834295390d73bb0cb07f5
Lenka Hobelova <hoblenka@gmail.com>
2025-12-18 16:25:37 +0100

    docker deploy + run tests

--- run.bat
+++ run.bat
@@ -9,6 +9,13 @@ if "%1"=="docker" (
         docker-compose build --no-cache
         docker-compose up -d
     )
+) else if "%1"=="hub" (
+    docker-compose down
+    if exist .env.docker (
+        docker-compose --env-file .env.docker -f docker-compose.hub.yml up -d
+    ) else (
+        docker-compose -f docker-compose.hub.yml up -d
+    )
 ) else (
     set DB_USERNAME_MYSQL=root
     set DB_PASSWORD_MYSQL=your_mysql_password
